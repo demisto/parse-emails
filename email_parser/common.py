470d42ab2427b229129103d65d87d97f953346eb
mgalitzki <mgalitzki@paloaltonetworks.com>
2021-09-02 08:45:23 +0300

    test commit

---
+++ email_parser/common.py
@@ -0,0 +1,48 @@
+from email.header import decode_header
+import re
+
+MIME_ENCODED_WORD = re.compile(r'(.*)=\?(.+)\?([B|Q])\?(.+)\?=(.*)')  # guardrails-disable-line
+ENCODINGS_TYPES = set(['utf-8', 'iso8859-1'])
+
+
+def convert_to_unicode(s, is_msg_header=True):
+    global ENCODINGS_TYPES
+    try:
+        res = ''  # utf encoded result
+        if is_msg_header:  # Mime encoded words used on message headers only
+            try:
+                word_mime_encoded = s and MIME_ENCODED_WORD.search(s)
+                if word_mime_encoded:
+                    word_mime_decoded = mime_decode(word_mime_encoded)
+                    if word_mime_decoded and not MIME_ENCODED_WORD.search(word_mime_decoded):
+                        # ensure decoding was successful
+                        return word_mime_decoded
+            except Exception as e:
+                # in case we failed to mine-decode, we continue and try to decode
+                print('Failed decoding mime-encoded string: {}. Will try regular decoding.'.format(str(e)))
+        for decoded_s, encoding in decode_header(s):  # return a list of pairs(decoded, charset)
+            if encoding:
+                res += decoded_s.decode(encoding)
+                ENCODINGS_TYPES.add(encoding)
+            else:
+                res += decoded_s.decode('utf-8', 'ignore')
+        return res.strip()
+    except Exception as e:
+        for file_data in ENCODINGS_TYPES:
+            try:
+                s = s.decode(file_data).encode('utf-8').strip()
+                break
+            except:  # noqa: E722
+                pass
+    if isinstance(s, bytes):
+        return s.decode('utf-8', 'ignore')
+    return s
+
+
+def mime_decode(word_mime_encoded):
+    prefix, charset, encoding, encoded_text, suffix = word_mime_encoded.groups()
+    if encoding.lower() == 'b':
+        byte_string = base64.b64decode(encoded_text)
+    elif encoding.lower() == 'q':
+        byte_string = quopri.decodestring(encoded_text)
+    return prefix + byte_string.decode(charset) + suffix
---
+++ email_parser/email_parser.py
@@ -0,0 +1,173 @@
+from email_parser.constants import MAX_DEPTH_CONST
+from email_parser.handle_msg import handle_msg
+from email_parser.handle_eml import handle_eml
+import traceback
+from base64 import b64decode
+
+
+class EmailParser(object):
+    """
+    The core class for the EmailParser.
+    """
+    def __init__(self, file_path, max_depth=3, parse_only_headers=False, file_type='', file_name=None):
+        self._file_path = file_path
+        self._file_type = file_type
+        self._file_name = file_name
+        self._max_depth = max_depth
+        self._parse_only_headers = parse_only_headers
+        self.parsed_email = None
+
+    def email_parser(self):
+        self.parsed_email = parse_email_files(file_path=self._file_path, max_depth=self._max_depth,
+                            parse_only_headers=self._parse_only_headers, file_type=self._file_type, file_name=self._file_name)
+
+
+def parse_email_files(file_path, max_depth=3, parse_only_headers=False, file_type='', file_name=None):
+    # we use the MAX_DEPTH_CONST to calculate the depth of the email
+    # each level will reduce the max_depth by 1
+    # not the best way to do it
+    global MAX_DEPTH_CONST
+    MAX_DEPTH_CONST = max_depth
+
+    if max_depth < 1:
+        raise Exception('Minimum max_depth is 1, the script will parse just the top email')
+
+    try:
+        file_type_lower = file_type.lower()
+        if 'composite document file v2 document' in file_type_lower \
+                or 'cdfv2 microsoft outlook message' in file_type_lower:
+            email_data, attached_emails = handle_msg(file_path, file_name, parse_only_headers, max_depth)
+            output = create_email_output(email_data, attached_emails)
+
+        elif any(eml_candidate in file_type_lower for eml_candidate in
+                 ['rfc 822 mail', 'smtp mail', 'multipart/signed', 'multipart/alternative', 'multipart/mixed', 'message/rfc822',
+                  'application/pkcs7-mime', 'multipart/related']):
+            if 'unicode (with bom) text' in file_type_lower:
+                email_data, attached_emails = handle_eml(
+                    file_path, False, file_name, parse_only_headers, max_depth, bom=True
+                )
+            else:
+                email_data, attached_emails = handle_eml(file_path, False, file_name, parse_only_headers, max_depth)
+            output = create_email_output(email_data, attached_emails)
+
+        elif ('ascii text' in file_type_lower or 'unicode text' in file_type_lower
+              or ('data' == file_type_lower.strip() and file_name and file_name.lower().strip().endswith('.eml'))):
+            try:
+                # Try to open the email as-is
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    file_contents = f.read()
+
+                if file_contents and 'Content-Type:'.lower() in file_contents.lower():
+                    email_data, attached_emails = handle_eml(file_path, b64=False, file_name=file_name,
+                                                             parse_only_headers=parse_only_headers, max_depth=max_depth)
+                    output = create_email_output(email_data, attached_emails)
+                else:
+                    # Try a base64 decode
+                    b64decode(file_contents)
+                    if file_contents and 'Content-Type:'.lower() in file_contents.lower():
+                        email_data, attached_emails = handle_eml(file_path, b64=True, file_name=file_name,
+                                                                 parse_only_headers=parse_only_headers,
+                                                                 max_depth=max_depth)
+                        output = create_email_output(email_data, attached_emails)
+                    else:
+                        try:
+                            # Try to open
+                            email_data, attached_emails = handle_eml(file_path, b64=False, file_name=file_name,
+                                                                     parse_only_headers=parse_only_headers,
+                                                                     max_depth=max_depth)
+                            is_data_populated = is_email_data_populated(email_data)
+                            if not is_data_populated:
+                                raise Exception("No email_data found")
+                            output = create_email_output(email_data, attached_emails)
+                        except Exception as e:
+                            # demisto.debug("ParseEmailFiles failed with {}".format(str(e)))
+                            raise Exception("Could not extract email from file. Possible reasons for this error are:\n"
+                                         "- Base64 decode did not include rfc 822 strings.\n"
+                                         "- Email contained no Content-Type and no data.")
+
+            except Exception as e:
+                raise Exception("Exception while trying to decode email from within base64: {}\n\nTrace:\n{}"
+                                .format(str(e), traceback.format_exc()))
+        else:
+            raise Exception("Unknown file format: [{}] for file: [{}]".format(file_type, file_name))
+        output = recursive_convert_to_unicode(output)
+        email = output  # output may be a single email
+        if isinstance(output, list) and len(output) > 0:
+            email = output[0]
+        return output
+
+        # before the change from demisto
+        # return_outputs(
+        #     readable_output=data_to_md(email, file_name, print_only_headers=parse_only_headers),
+        #     outputs={
+        #         'Email': output
+        #     },
+        #     raw_response=output
+        # )
+
+    except Exception as ex:
+        raise Exception(str(ex) + "\n\nTrace:\n" + traceback.format_exc())
+
+
+def create_email_output(email_data, attached_emails):
+    # for backward compatibility if there are no attached files we return single dict
+    # if there are attached files then we will return array of all the emails
+    res = []
+    if email_data:
+        res.append(email_data)
+    if len(attached_emails) > 0:
+        res.extend(attached_emails)
+    if len(res) == 0:
+        return None
+    if len(res) == 1:
+        return res[0]
+    return res
+
+
+def is_email_data_populated(email_data):
+    # checks if email data has any item populated to it
+    if email_data:
+        for key, val in email_data.items():
+            if val:
+                return True
+    return False
+
+
+def recursive_convert_to_unicode(replace_to_utf):
+    """Converts object into UTF-8 characters
+    ignores errors
+    Args:
+        replace_to_utf (object): any object
+
+    Returns:
+        object converted to UTF-8
+    """
+    try:
+        if isinstance(replace_to_utf, dict):
+            return {recursive_convert_to_unicode(k): recursive_convert_to_unicode(v) for k, v in replace_to_utf.items()}
+        if isinstance(replace_to_utf, list):
+            return [recursive_convert_to_unicode(i) for i in replace_to_utf if i]
+        if isinstance(replace_to_utf, str):
+            return str(replace_to_utf, encoding='utf-8', errors='ignore')
+        if not replace_to_utf:
+            return replace_to_utf
+        return replace_to_utf
+    except TypeError:
+        return replace_to_utf
+
+
+
+
+test_path = '/Users/mgalitzki/Desktop/attachmentwithindicators.msg'
+test_type = 'cdfv2 microsoft outlook message'
+test_name = 'attachmentwithindicators.msg'
+
+
+# test_path = '/Users/mgalitzki/dev/demisto/content/Packs/CommonScripts/Scripts/ParseEmailFiles/test_data/eml_contains_base64_eml.eml'
+# test_type = "RFC 822 mail text, with CRLF line terminators"
+# test_name = 'eml_contains_base64_eml.eml'
+
+aa = EmailParser(file_path=test_path, max_depth=3, parse_only_headers=False, file_type=test_type, file_name=test_name)
+aa.email_parser()
+
+print(aa.parsed_email)
