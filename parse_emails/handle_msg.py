2b2861e60f710cc047ce96e1aeec254bb195eb16
mgalitzki <mgalitzki@paloaltonetworks.com>
2022-01-16 08:59:20 +0200

    update pipfile

--- parse_emails/handle_msg.py
+++ parse_emails/handle_msg.py
@@ -820,8 +820,6 @@ class Message(object):
         self.body = property_values.get("Body")
 
         if "RtfCompressed" in property_values:
-            with open('test_compressed.rtf', mode='wb') as f:
-                f.write(property_values['RtfCompressed'])
             try:
                 import compressed_rtf
             except ImportError:
@@ -829,27 +827,13 @@ class Message(object):
             if compressed_rtf:
                 compressed_rtf_body = property_values['RtfCompressed']
                 self.body = compressed_rtf.decompress(compressed_rtf_body)
-            #     rtf_body = compressed_rtf.decompress(compressed_rtf_body)
-            #
-            #     path = 'test.rtf'
-            #     with open('test.rtf', mode='w') as f:
-            #         f.write(str(rtf_body, 'utf-8', 'ignore'))
-            #
-            #     # run_cmd = ['soffice', '--headless', '--norestore', '--convert-to', 'html', path]
-            #
-            #     # env = os.environ.copy()
-            #     # env['HOME'] = '/tmp/convertfile'
-            #     # res = subprocess.check_output(run_cmd, stderr=subprocess.STDOUT, universal_newlines=True, timeout=)
-            #     # logging.debug("completed running: {}. With result: {}".format(run_cmd, res))
-            #
+
                 from RTFDE.deencapsulate import DeEncapsulator
 
                 rtf_obj = DeEncapsulator(self.body)
                 rtf_obj.deencapsulate()
                 if rtf_obj.content_type == 'html':
-                    with open('test_rtfde.html', 'w') as f:
-                        f.write(rtf_obj.html)
-                        self.html = rtf_obj.html
+                    self.html = rtf_obj.html
 
     def _set_recipients(self):
         recipients = self.recipients
