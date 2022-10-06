7be93021a8e0f841dbed0be521ad184dd1714517
GuyAfik <guyafik11@gmail.com>
2022-10-06 11:13:23 +0300

    add logging

--- parse_emails/parse_emails.py
+++ parse_emails/parse_emails.py
@@ -39,8 +39,11 @@ class EmailParser(object):
         mime = magic.Magic()
         if not file_type:
             file_type = mime.from_file(self._file_path)
+        
+        logging.debug(f'{file_type=}')
 
         if file_type == 'data' and self._file_name.lower().strip().endswith('.p7m'):
+            logging.info(f'Removing signature from p7m file {self._file_path}')
             bio = remove_p7m_file_signature(self._file_path)
             if bio:
                 with open(self._file_path, 'w') as fp:  # override the contents of the .p7m file without the signature.
