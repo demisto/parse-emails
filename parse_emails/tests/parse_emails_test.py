2afb07f10dbdb73403db5b06300315651f1ab406
Moshe <mgalitzki@paloaltonetworks.com>
2025-04-20 11:58:19 +0300

    added test

--- parse_emails/tests/parse_emails_test.py
+++ parse_emails/tests/parse_emails_test.py
@@ -1013,3 +1013,19 @@ def test_multipart_eml_with_eml_attachment_containing_html_body():
     assert results[0]["Attachments"] == "original_message.eml"
     assert len(results[0]["AttachmentsData"]) > 0
     assert results[1]["ParentFileName"] == "multipart_with_eml_attachment_containing_html.eml"
+
+
+def test_error_file_type():
+    """
+    Given:
+     - eml file Sent with file_info='AMUSIC Adlib Tracker'.
+    When:
+     - parsing the file.
+    Then:
+     - make sure the eml was correctly parsed.
+    """
+    test_path = 'parse_emails/tests/test_data/None_Funding..eml'
+
+    email_parser = EmailParser(file_path=test_path, max_depth=2, file_info='AMUSIC Adlib Tracker')
+    results = email_parser.parse()
+    assert results['Subject'] == 'None Funding.'
